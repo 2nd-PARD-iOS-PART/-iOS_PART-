8fef6660ba9c6108d313127f584607ef759f9339
haram22 <kimhr9889@handong.ac.kr>
2023-09-27 15:01:20 +0900

    add tableView

---
+++ study_ios_tableview/tableview_study/Democell.swift
@@ -0,0 +1,28 @@
+//
+//  Democell.swift
+//  tableview_study
+//
+//  Created by 진세진 on 2023/07/24.
+//
+
+import UIKit
+
+class Democell : UITableViewCell{
+    let textlabel = UILabel()
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: "cell")
+        contentView.addSubview(textlabel)
+        setUplabel()
+        
+    }
+    func setUplabel(){
+        textlabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            textlabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            textlabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 15)
+        ])
+    }
+}
